fcac993884afaead50e2934888baa7bc2bc4b3e8
Josef8852 <josefaboshady2@gmail.com>
2026-03-11 19:32:11 +0100

    Implement Protected Route -> Dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import useAuth from "../context/useAuth";
+
+function Dashboard() {
+  
+  const { user } = useAuth();
+  
+  
+  return (
+    <section className="min-h-[70vh] bg-white py-12 px-4 mt-15 flex flex-col items-center justify-start">
+      <div class=" w-auto inline-block px-4 py-2 font-mono font-bold bg-green-400 border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] uppercase">
+        <h1 className="text-3xl"> Welcome <span class="text-white">{user}</span></h1>
+      </div>
+    </section>
+  )
+  
+}
+
+
+
+export default Dashboard; 
